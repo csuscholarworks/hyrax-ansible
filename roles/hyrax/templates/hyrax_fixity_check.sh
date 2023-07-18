5244935f191325204c4c504d38e000cb135a6c9b
Rob Kaufman <rob@notch8.com>
2023-07-18 14:58:45 -0700

    fixity check and release cron jobs

--- roles/hyrax/templates/hyrax_fixity_check.sh
+++ roles/hyrax/templates/hyrax_fixity_check.sh
@@ -2,4 +2,4 @@
 
 cd /var/www/hyrax/current/
 set -o allexport; source /var/www/hyrax/.env; set +o allexport
-(echo -n "$(date --rfc-3339=seconds) " && /usr/local/bin/bundle exec rails runner "Hyrax::RepositoryFixityCheckService.fixity_check_everything" && echo "success" || echo "failure") >> /var/log/hyrax/fixitycheck.log 2>&1
+(echo -n "$(date --rfc-3339=seconds) " && /usr/local/bin/bundle exec rails calstate:all_files_fixity_check && echo "success" || echo "failure") >> /var/log/hyrax/fixitycheck.log 2>&1
